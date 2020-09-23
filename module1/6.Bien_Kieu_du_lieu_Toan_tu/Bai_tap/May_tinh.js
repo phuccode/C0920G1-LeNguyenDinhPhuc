210f8d59ab8898e388b0a707700047fefa9682a7
PC <phucle.110898@gmail.com>
2020-09-23 13:29:24 +0700

    nopbai

---
+++ module1/6.Bien_Kieu_du_lieu_Toan_tu/Bai_tap/May_tinh.js
@@ -0,0 +1,42 @@
+
+
+
+function Addition(){
+    let inputA = parseInt(document.getElementById("inputA").value);
+    let inputB = parseInt(document.getElementById("inputB").value);
+    let addition;
+    addition = inputA + inputB;
+    document.getElementById("result").innerText= "Result Division: " + addition;
+
+
+}
+
+function Multiplication(){
+    let inputA = parseInt(document.getElementById("inputA").value);
+    let inputB = parseInt(document.getElementById("inputB").value);
+    let addition;
+    addition = inputA * inputB;
+    document.getElementById("result").innerText= "Result Division: " + addition;
+}
+
+function Subtraction(){
+    let inputA = parseInt(document.getElementById("inputA").value);
+    let inputB = parseInt(document.getElementById("inputB").value);
+    let addition;
+    addition = inputA - inputB;
+    document.getElementById("result").innerText= "Result Division: " + addition;
+}
+
+function Division(){
+    let inputA = parseInt(document.getElementById("inputA").value);
+    let inputB = parseInt(document.getElementById("inputB").value);
+    let addition;
+    addition = inputA / inputB;
+    document.getElementById("result").innerText= "Result Division: " + addition;
+}
+
+
+
+
+
+
